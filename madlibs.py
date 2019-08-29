5f0387fe7f8d3aac66b6f0cf4ddcb605d9d3bb5c
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-08-29 10:48:20 -0700

    Completed requirement: collect user input

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,14 @@
-user_input = {
-    "nouns": [],
-    "adjectives": [],
-    "verbs": []
-}
+# import random
+
+story_input = ["noun", "verb", "noun", "adjective"]
+
+user_input = {}
+
+for part_of_speech in story_input:
+    user_word = input("Enter a {}: ".format(part_of_speech)).lower()
+    if part_of_speech not in user_input:
+        user_input[part_of_speech] = list()
+    user_input[part_of_speech].append(user_word)
+
+# story = "My name is {}. I am {} to get {} ice cream.".format()
+# print(user_input)
