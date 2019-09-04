80db430fb75c48cd97760301d2e46b2150ebee37
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-09-03 22:48:37 -0700

    completed stretch challenges 'handle improper input' and 'story formatting'

---
+++ README.md
@@ -0,0 +1,36 @@
+# Madlibs
+
+## Description
+Mad Libs typically starts by prompting you (the user) to provide words based on the various parts of speech:, typically adjectives, nouns, and verbs. Once you have provided those words, they get placed into a short story with blanks for the specific parts of speech (i.e. if you’re prompted for two adjectives and one verb, the story will have two blanks for adjectives and one blank for a verb).
+
+## Goals
+### Requirements
+- [x] Provide a Mad Libs story for users to fill in
+- [x] Collect user input for the words
+- [x] Print the story with all blanks filled in by the user’s words
+- [x] Use at least 3 parts of speech for word replacement
+- [x] Use variable assignment to store/use values
+- [x] Write and call functions
+- [x] Use at least one of the following core data types: strings, integers, floats
+- [x] Use at least one of the following collection types: lists, tuples, dictionary
+
+
+### Stretch Challenges
+- [x] Handles improper user input
+- [x] Story uses formatting and colors to provide a richer experience
+- [x] The story uses 4+ parts of speech
+- [x] Randomize the words of the same type (ie shuffle the 5 nouns in terms of which noun blanks they go into).
+- [ ] Use a list of words from a dictionary to generate words randomly if the user requests it.
+- [ ] Use a different data structure to store words.
+- [x] Build with test driven development (TDD)
+- [ ] Use the system module (for accessing command-line arguments)
+- [ ] Add ASCII art/design to your story
+- [ ] Take Mad Libs out of the terminal and have it running as a website
+
+
+## How to run/setup
+Clone or download this repo, go to the directory where the downloaded file is, then type in the terminal `python3 madlibs.py`
+
+## Resources
+Avatar Wiki for Opening Sequence: https://avatar.fandom.com/wiki/Opening_sequence
+Stack Overflow of course
--- madlibs.py
+++ madlibs.py
@@ -1,33 +1,50 @@
 import random
-
-user_input = {}
-word_count = {}
-missing_words = {}
-
-story = "_1NAME_ likes to _1VERB_ ice cream. She prefers _1ADJECTIVE_ over _2ADJECTIVE_. _1NAME_ went to the store today to buy some _1ADJECTIVE_ ice cream, but they only had _2ADJECTIVE_ ice cream."
+from termcolor import colored
 
 def get_user_input(missing_word):
-    if missing_word[0] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
-        user_word = input("Enter an {}: ".format(missing_word.lower()))
-    else:
-        user_word = input("Enter a {}: ".format(missing_word.lower()))
+    is_not_alpha_loop = True
+    while is_not_alpha_loop:
+        if missing_word[0] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
+            user_word = input("Enter an {}: ".format(missing_word.lower()))
+        else:
+            user_word = input("Enter a {}: ".format(missing_word.lower()))
+
+        # handles improper user input
+        if user_word.isalpha() == False:
+            print("Numbers and symbols are not supported. Please enter only letters.")
+        else:
+            is_not_alpha_loop = False
+
+    # appends user input to appropriate missing_word key in user_input
     if missing_word not in user_input:
         user_input[missing_word] = list()
-    user_input[missing_word].append(user_word)
+    user_input[missing_word].append(random_color(user_word))
 
+# picks random word from user_input
 def get_random_word(part_of_speech):
     word = user_input[part_of_speech][random.randint(0, len(user_input[part_of_speech]) - 1)]
     user_input[part_of_speech].remove(word)
     return word
 
-def create_story():
+def random_color(word):
+    colors = ["grey", "red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+    random_color_index = random.randint(0, len(colors)-1)
+    word = colored(word, colors[random_color_index])
+    return word
+
+def create_story(story):
     story_list = story.split(" ")
 
     for item in story_list:
-        if item[0] == "_" and item[len(item) - 1] == "_": # checks if word is a missing_word
-            missing_words[item] = ""
-            missing_word = item[2:len(item) - 1] # removes missing_word identifiers
+        if item[0] == "_": # checks if word is a missing_word
+            if item[len(item) - 1] == "_":
+                last_char = len(item) - 1
+            else:
+                last_char = len(item) - 2
+            missing_words[item[1:last_char]] = ""
+            missing_word = item[2:last_char] # removes missing_word identifiers
 
+            # checks if missing_word is a key in word_count, updates count if it is
             if missing_word not in word_count:
                 word_count[missing_word] = 1
                 get_user_input(missing_word)
@@ -35,20 +52,46 @@ def create_story():
                 word_count[missing_word] += 1
                 get_user_input(missing_word)
 
+    # chooses a random word for story
     for item in missing_words:
-        missing_words[item] = get_random_word(item[2:len(item) - 1])
+        missing_words[item] = get_random_word(item[1:])
 
+    # populates user's words back to story
     for index in range(len(story_list)):
         word = story_list[index]
-        if "." in word:
-            word = word[:len(word)-1]
-        if word in missing_words:
-            if "." in story_list[index]:
-                story_list[index] = missing_words[word] + "."
+        if word[0] == "_":
+            last_char = word[len(word)-1]
+            if last_char != "_":
+                word = word[1:len(word)-2]
+                story_list[index] = missing_words[word] + last_char
             else:
+                word = word[1:len(word)-1]
                 story_list[index] = missing_words[word]
 
     finished_story = " ".join(story_list)
+    print()
     return finished_story
 
-print(create_story())
+user_input = {} # {"PART OF SPEECH": "LIST -- USER WORDS"}
+word_count = {} # {"PART OF SPEECH": "INT -- COUNT"}
+missing_words = {} # {"PART OF SPEECH": "STRING -- RANDOM USER WORD"}
+
+story1 = "_1NAME_ likes to _1VERB_ ice cream. She prefers _1ADJECTIVE_ over _2ADJECTIVE_. _1NAME_ went to the store today to buy some _1ADJECTIVE_ ice cream, but they only had _2ADJECTIVE_ ice cream."
+
+story2 = "_1NOUN_. _2NOUN_. _3NOUN_. _4NOUN_. Long ago, four nations lived together in _5NOUN_. Then, everything changed when the _1ADJECTIVE_ _2NOUN_ Nation attacked. Only the _6NOUN_, master of all four elements, could _1VERB_ them, but when the world needed him most, he _2VERB_. A hundred years passed and my _7NOUN_ and I _3VERB_ the new _6NOUN_, a _2NOUN_ bender named _1NAME_. And although his _2NOUN_ bending skills are _2ADJECTIVE_, he has a lot to _4VERB_ before he's ready to _5VERB_ anyone. But I believe _1NAME_ can _5VERB_ the _8NOUN_."
+
+# print(create_story(story1)) # TEST
+print(create_story(story2))
+
+# -------------- WEBSITE --------------
+# def get_missing_words():
+#     story_list = story.split(" ")
+#
+#     for item in story_list:
+#         if item[0] == "_" and item[len(item) - 1] == "_": # checks if word is a missing_word
+#             missing_words[item] = ""
+#             missing_word = item[2:len(item) - 1] # removes missing_word identifiers
+#             if missing_word not in user_input:
+#                 user_input[missing_word] = list()
+#             user_input[missing_word].append("")
+#     return user_input
